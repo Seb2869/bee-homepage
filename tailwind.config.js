9c2aa1b470ae354b847de3ef500f1eb29c7c8f46
JainamShah28 <shahjainam.com.28@gmail.com>
2025-04-27 00:07:08 +0530

    feat: 🎨 add theme fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#F7AD33',
+                white: '#FFFFFF',
+                offWhite: '#FAFAFA',
+                black: '#14110E',
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+                anton: ['Anton', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
