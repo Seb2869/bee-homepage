eb4674ce6aa31380c29484761b87f0f40b7d5ad3
JainamShah28 <shahjainam.com.28@gmail.com>
2025-06-07 15:44:08 +0530

    feat: ✨ add responsive leaderboard section in build page

--- src/pages/Build.tsx
+++ src/pages/Build.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BuildHero } from '../components/build/BuildHero';
 import { Footer } from '../components/build/Footer';
 import { HowToListYourServer } from '../components/build/how-to-list-your-server/HowToListYourServer';
+import { LeaderBoard } from '../components/build/LeaderBoard';
 
 const Build: React.FC = () => {
     return (
         <div className="w-full min-h-full">
             <BuildHero />
             <HowToListYourServer />
+            <LeaderBoard />
             <Footer />
         </div>
     );
