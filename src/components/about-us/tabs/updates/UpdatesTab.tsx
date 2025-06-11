7b7356b9de1c724bfad99f977d99793824c3c807
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2025-06-11 13:18:23 +0530

    feat: updates for commandhive

--- src/components/about-us/tabs/updates/UpdatesTab.tsx
+++ src/components/about-us/tabs/updates/UpdatesTab.tsx
@@ -17,6 +17,70 @@ import X from '../../../../assets/about-us/updates/x.svg';
 export const UpdatesTab: React.FC = () => {
     const updatesData: Update[] = [
         {
+            date: 'June 11th, 2025',
+            items: [
+                {
+                    type: UpdateItemType.LIST,
+                    title: 'Highlights for this week!',
+                    icon: TrendingUp,
+                    points: [
+                        {
+                            id: 1,
+                            text: 'Completed our docker container and hosted image on dockerhub! Each user will have their own docker container which will run agents, store memory, knowledge base and logs.',
+                            links: [
+                                {
+                                    text: 'docker container',
+                                    url: 'https://github.com/CommandHive/bee-container',
+                                },
+                            ],
+                        },
+                        {
+                            id: 2,
+                            text: 'The endpoints of the docker container allow for creation, starting, stopping and deletion of an AI agent.',
+                        },
+                        {
+                            id: 3,
+                            text: 'We created an image for our docker container which can be used on Phala TEE, with total downloads of 60+.',
+                            links: [
+                                {
+                                    text: 'Phala TEE',
+                                    url: 'https://hub.docker.com/r/vaibhavgeek/user-agent-api',
+                                },
+                            ],
+                        },
+                        {
+                            id: 4,
+                            text: 'Created a page for new ecosystem builders to contribute to CommandHive by MCP Servers - where they are funded by Quadratic Funding.',
+                            links: [
+                                {
+                                    text: 'ecosystem builders to contribute',
+                                    url: 'https://commandhive.xyz/build',
+                                },
+                            ],
+                        },
+                        {
+                            id: 5,
+                            text: 'We improved the documentation for our pip package and added a video for other people to build and use it. (https://pypi.org/project/bee-agent/), it has now 50 total downloads.',
+                            links: [
+                                {
+                                    text: 'documentation for our pip package',
+                                    url: 'https://pypi.org/project/bee-agent/',
+                                },
+                            ],
+                        },
+                        {
+                            id: 6,
+                            text: 'Our application with containerized dockerization will launch on June 18th! So we are gearing up for the launch.',
+                        },
+                        {
+                            id: 7,
+                            text: 'We will be attending Berlin Blockchain Week and then EthCC at Cannes, come say hi!',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
             date: 'June 1st, 2025',
             items: [
                 {
